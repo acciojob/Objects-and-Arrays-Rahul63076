99e40cffa2a5f4ac447165e070ab80c5fc76cb99
Rahul Shukla <114052202+Rahul63076@users.noreply.github.com>
2023-06-16 09:32:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,23 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+// players array
+var players = ["John", "Mike", "Sarah"];
+
+// person object
+var person = {
+  name: "Alice",
+  age: 25,
+};
+
+// Create a reference to players array
+var team = players;
+
+// Create a copy of players array
+var team1 = players.slice();
+
+// Create a copy of person object
+var cap1 = Object.assign({}, person);
 
 window.players = players;
 window.person = person;
